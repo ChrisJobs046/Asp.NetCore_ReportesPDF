07f04fc98ee633b294f88bc0c5778ccef5749c1a
ChrisJobs046 <ChrisJobs046@gmail.com>
2020-07-22 15:30:28 -0400

    Probando

---
+++ WebReportesPDF/Controllers/CrearPDFController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Rotativa.AspNetCore;
+
+namespace WebReportesPDF.Controllers
+{
+    public class CrearPDFController : Controller
+    {
+        public IActionResult Index()
+        {
+            //Este metodo se crea, al instalar Rotativa.Asp.NetCore
+            return new ViewAsPdf("Index")
+            {
+
+            };
+        }
+    }
+}
